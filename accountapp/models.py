62a8195bdf3a0715ed72d2466800bbdde610a042
whycomere <alxjfkd123@naver.com>
2021-07-08 09:46:39 +0900

    2021.07.05 5반 최정환입니다..!!

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False)
+
+# null = false 이유는 텍스트가하나밖에없어서 다 사라질수도 있다잉
--- accountapp/views.py
+++ accountapp/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "post":
+        return render(request, 'accountapp/hello_world.html', context={'text': 'post method!'})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'get method!'})
 
+
+# def hello_world(request):
+#     return render(request, 'accountapp/hello_world.html')
